a832a4f311065ae6f5925445ac2e09f29aae36bd
Gus Jianas <gjianas@gmail.com>
2013-01-21 22:06:32 -0500

    all tabs removed from yml files

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -18,25 +18,25 @@ class UserTest < ActiveSupport::TestCase
      assert !user.errors[:profile_name].empty?
  end
 
-	 test "a user should have a unique profile name" do
- 	 user= User.new
-	 user.profile_name = "gjianas"
-	 user.first_name="Gus"
-	 user.last_name="Jianas"
-	 user.password="mulax028"
-	 user.password_confirmation="mulax028"
-	 user.email="gjianas@gmail.com"
-
- 	assert !user.save
-	 assert !user.errors[:profile_name] .empty? 	
+     test "a user should have a unique profile name" do
+      user= User.new
+     user.profile_name = "gjianas"
+   user.email="gjianas@gmail.com"
+     user.first_name="Gus"
+     user.last_name="Jianas"
+     user.password="mulax028"
+     user.password_confirmation="mulax028"
+     
+     assert !user.save
+     assert !user.errors[:profile_name] .empty?     
  end
 
-	test "a user should have a profile name without spaces" do
- 	user = User.new
- 	user.profile_name = "spces in profile"
+    test "a user should have a profile name without spaces" do
+     user = User.new
+     user.profile_name = "spces in profile"
 
- 	assert !user.save
- 	assert !user.errors[:profile_name].empty?
- 	assert !user.errors[:profile_name].include?("Must be formatted correctly.")
-	end
-end	
+     assert !user.save
+     assert !user.errors[:profile_name].empty?
+     assert !user.errors[:profile_name].include?("Must be formatted correctly.")
+    end
+end    
